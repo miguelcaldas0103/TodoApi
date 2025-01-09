94340475533fe58651e3567c77725664517b80a3
mamcgc <miguelamcgcaldas@gmail.com>
2025-01-09 22:16:43 +0000

    Adding delete button; TODO: Refactor .NET API and add markAsComplete endpoint and remove not needed endpoints.

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -16,6 +16,36 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
     }
 
+    const deleteTodo = async (id) => {
+        try {
+            const response = await fetch(`http://127.0.0.1:5271/todoitems/${id}`, {
+                method: 'DELETE',
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to delete todo. Status: ${response.status}`);
+            }
+
+            return response.status; // Returns 200 if successful
+        } catch (error) {
+            console.error("Error deleting todo:", error);
+            throw error; // Propagate the error if needed
+        }
+    };
+
+    // const markAsCompleted = async (id) => {
+    //     try {
+    //         response = await fetch(`http://127.0.0.1:5271/todoitems/${id}`, {
+    //             method: 'PUT'
+    //         })
+    //         if (!response.ok) {
+
+    //         }
+    //     } catch (error) {
+
+    //     }
+    // }
+
     const renderTodos = async () => {
         const todos = await getApplicationTodos();
 
@@ -26,9 +56,19 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
         else {
             todos.forEach(todo => {
-                const todoItem = document.createElement('div');
+                const todoItem = document.createElement('div')
+                const deleteButton = document.createElement('button')
+
+                deleteButton.addEventListener('click', async (params) => {
+                    await deleteTodo(todo.id);
+                    renderTodos();
+                })
+
+                deleteButton.innerHTML = 'Delete'
+
                 todoItem.classList.add('todo-item');
                 todoItem.innerHTML = `<h3>${todo.name}</h3>`
+                todoItem.appendChild(deleteButton)
                 todosContainer.appendChild(todoItem);
             });
         }
